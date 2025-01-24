bdb987406c33b3842d5d9ce2cf52ffa0eb08f22a
David R <david2005.rosental@gmail.com>
2025-01-24 15:54:38 +0000

    Initial commit with project setup and updated README.md with prerequisites.

---
+++ junctionmodeller/src/main/java/uk/ac/warwick/dcs/junctionmodeller/App.java
@@ -0,0 +1,10 @@
+package uk.ac.warwick.dcs.junctionmodeller;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
