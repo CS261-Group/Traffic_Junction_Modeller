f6878d27173d39a1f87ac1a955f99cb9fb1ac58c
JachymT <Jachym.tolar@gmail.com>
2025-02-02 23:54:42 +0000

    updated plan
    Removed style and function. Added boxed.

--- formdesign.js
+++ formdesign.js
@@ -5,31 +5,48 @@ $(() => {
     for (const direction of ["n", "e", "s", "w"]) {
         const dropdownIn = `#${direction}numlanesin`;
         const listIn = `#${direction}inlanes`;
+        const listDir = `#${direction}dirlanes`;
+        const listOut = `#${direction}outlanes`;
         $(dropdownIn).on("change", () => {
             const numLanes = Number.parseInt($(`${dropdownIn} option:selected`).val());
             console.log(numLanes);
     
             // remove all previous
             $(listIn).empty();
-    
-            // add new lanes
-            for (let i = 0; i < numLanes; i++) {
-                $(listIn).append(`<li class="laneconf">Lane #${i + 1}</li>`);
-            }
-        });
-
-        const dropdownOut = `#${direction}numlanesout`;
-        const listOut = `#${direction}outlanes`;
-        $(dropdownOut).on("change", () => {
-            const numLanes = Number.parseInt($(`${dropdownOut} option:selected`).val());
-            console.log(numLanes);
-    
-            // remove all previous
+            $(listDir).empty();
             $(listOut).empty();
     
             // add new lanes
             for (let i = 0; i < numLanes; i++) {
-                $(listOut).append(`<li class="laneconf">Lane #${i + 1}</li>`);
+                $(listIn).append(`<div class"inlaneconf"> <li> Lane #${i + 1}</li>
+                    <input type="number" value="100">
+                </div>`);
+            }
+
+            // add new lanes
+            for (let i = 0; i < numLanes; i++) {
+                $(listDir).append(`<div class"dirlaneconf"> <li> Lane #${i + 1}
+                        <div>
+                            <input type="checkbox" value="east" />
+                            <label for="east"><em>Eastbound</em> &#8594;</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" value="north" />
+                            <label for="north"><em>Northbound</em>&#8593;</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" value="west" />
+                            <label for="west"><em>Westbound</em> &#8592;</label>
+                        </div>
+                        </li>
+                </div>`);
+            }
+
+            // add new lanes
+            for (let i = 0; i < numLanes; i++) {
+                $(listOut).append(`<div class"outlaneconf"> <li> Lane #${i + 1}</li>
+                    <input type="number" value="100">
+                </div>`);
             }
         });
     }
