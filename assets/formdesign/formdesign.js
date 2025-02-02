682cf547097737e2a632fd9d9e2dd32c1b6a8423
David R <david2005.rosental@gmail.com>
2025-02-02 01:33:20 +0000

    Some form design attempted, I also created the first version of the data processing component's UML class diagram (but I still need to make the UML for the contracts library and the Model component)

--- assets/formdesign/formdesign.js
+++ assets/formdesign/formdesign.js
@@ -1,35 +1,49 @@
 
 $(() => {
+    // event listener for checkboxes for directions
+    $("#noutlanes").on("click", ".directionchbox", () => {
+        if (!$(this).is(":checked")) {
+            const name = $(this).attr("name");
+            console.log(name);
+            // const num = name.substr(1, name.length - 3);
+            // console.log(num);
+        }
+    });
+
     // when the number of lanes changes, we must truncate or generate the other lanes
     // dynamically
-    for (const direction of ["n", "e", "s", "w"]) {
-        const dropdownIn = `#${direction}numlanesin`;
-        const listIn = `#${direction}inlanes`;
-        $(dropdownIn).on("change", () => {
-            const numLanes = Number.parseInt($(`${dropdownIn} option:selected`).val());
-            console.log(numLanes);
-    
-            // remove all previous
-            $(listIn).empty();
-    
-            // add new lanes
-            for (let i = 0; i < numLanes; i++) {
-                $(listIn).append(`<li class="laneconf">Lane #${i + 1}</li>`);
-            }
-        });
-
+    for (const direction of ["n"]) {
         const dropdownOut = `#${direction}numlanesout`;
-        const listOut = `#${direction}outlanes`;
         $(dropdownOut).on("change", () => {
             const numLanes = Number.parseInt($(`${dropdownOut} option:selected`).val());
-            console.log(numLanes);
     
-            // remove all previous
-            $(listOut).empty();
-    
-            // add new lanes
+            // this shit below just didn't work with jquery idk why
+            const table = document.getElementById("noutlanes");
+            
+            table.innerHTML = ""; // Clear existing table
+            
+            const headerRow = table.insertRow();
             for (let i = 0; i < numLanes; i++) {
-                $(listOut).append(`<li class="laneconf">Lane #${i + 1}</li>`);
+                const th = document.createElement("th");
+                th.textContent = `Lane ${i + 1}`;
+                headerRow.appendChild(th);
+            }
+            
+            const dataRow = table.insertRow();
+            for (let i = 0; i < numLanes; i++) {
+                const td = document.createElement("td");
+                td.innerHTML = `
+                <strong>Available outbound directions:</strong>
+                <br>
+                <label for="n${i+1}toe">&larr;</label>
+                <input class="directionchbox" type="checkbox" id="n${i+1}toe" name="n${i+1}toe" checked>
+                <label for="n${i+1}toe">&darr;</label>
+                <input class="directionchbox" type="checkbox" id="n${i+1}tos" name="n${i+1}tos" checked>
+                <label for="n${i+1}toe">&rarr;</label>
+                <input class="directionchbox" type="checkbox" id="n${i+1}tow" name="n${i+1}tow" checked>
+                <br>
+                `
+                dataRow.appendChild(td);
             }
         });
     }
@@ -49,16 +63,21 @@ $(() => {
 
     $("#npedcrossing").trigger("change");
 
-    $("#neditcrossing").on('click', (e) => {
-        e.preventDefault();
-        // TODO: pop up
-        console.log("EDIT CROSSING POPUP");
-    });
+    // modal pop-up window setups
+    const modalSetups = [
+        ["#edittrafficlight", "#closetrafficlightmodal", "#trafficlightmodal"],
+        ["#neditcrossing", "#nclosecrossingsettingsmodal", "#ncrossingsettingsmodal"]
+    ];
 
-    $("#nedittrafficlight").on('click', (e) => {
-        e.preventDefault();
-        // TODO: pop up
-        console.log("EDIT TRAFFIC LIGHT POPUP");
-    });
+    for (const [openBtn, closeBtn, modal] of modalSetups) {
+        $(openBtn).on('click', (e) => {
+            e.preventDefault();
+            $(modal).css({display: "block"});
+        });
     
+        $(closeBtn).on('click', (e) => {
+            e.preventDefault();
+            $(modal).css({display: "none"});
+        }); 
+    }
 });
