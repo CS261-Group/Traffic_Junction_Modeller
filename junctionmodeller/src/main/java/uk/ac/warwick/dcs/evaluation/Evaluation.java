c2a6456ee60c3349277b241ba39cdde5faa17614
JachymT <Jachym.tolar@gmail.com>
2025-02-09 15:43:18 +0000

    created evaluation methods

---
+++ junctionmodeller/src/main/java/uk/ac/warwick/dcs/evaluation/Evaluation.java
@@ -0,0 +1,17 @@
+package uk.ac.warwick.dcs.evaluation;
+
+import uk.ac.warwick.dcs.contracts.JunctionConfiguration;
+
+public class Evaluation {
+    public static float averageLaneDelay(JunctionConfiguration junction){
+        return 0;
+    }
+
+    public static float MaximumLaneDelay(JunctionConfiguration junction){
+        return 0;
+    }
+
+    public static int MaximumQueueLength(JunctionConfiguration junction){
+        return 0;
+    }
+}
--- junctionmodeller/src/main/java/uk/ac/warwick/dcs/evaluation/LaneAverageDelay.java
+++
@@ -1,5 +0,0 @@
-package uk.ac.warwick.dcs.evaluation;
-
-public class LaneAverageDelay {
-    public static void getAverageDelay(){}
-}
