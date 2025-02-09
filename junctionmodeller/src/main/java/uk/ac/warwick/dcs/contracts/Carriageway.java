13b7937de7233951317e60e04400fdcf127a3d2c
JachymT <Jachym.tolar@gmail.com>
2025-02-09 15:24:33 +0000

    changes to contracts

--- junctionmodeller/src/main/java/uk/ac/warwick/dcs/contracts/Carriageway.java
+++ junctionmodeller/src/main/java/uk/ac/warwick/dcs/contracts/Carriageway.java
@@ -1,7 +1,5 @@
 package uk.ac.warwick.dcs.contracts;
 
-import uk.ac.warwick.dcs.contracts.lights.TrafficLight;
-
 // traffic lights now belong to Incoming Roads
 // there may now be many incoming roads in a carriageway
 public class Carriageway {
