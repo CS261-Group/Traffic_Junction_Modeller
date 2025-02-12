918af45c857b61192741ddf0f90b8a4c0f5177b7
satyan12 <satyan.bhundia@hotmail.com>
2025-02-12 18:22:05 +0000

    Create Optimiser.java

---
+++ junctionmodeller/src/main/java/uk/ac/warwick/dcs/optimisation/Optimiser.java
@@ -0,0 +1,49 @@
+package uk.ac.warwick.dcs.optimisation;
+
+public class Optimiser {
+
+    private ActuatedTimingsOptimiser actuatedTimingsOptimiser;
+    private FixedTimingsOptimiser fixedTimingsOptimiser;
+    private CycleTimeOptimiser cycleTimeOptimiser;
+
+    public Optimiser() {
+        this.actuatedTimingsOptimiser = new ActuatedTimingsOptimiser();
+        this.fixedTimingsOptimiser = new FixedTimingsOptimiser();
+        this.cycleTimeOptimiser = new CycleTimeOptimiser();
+    }
+
+    public int[] getMaxGroupTimings(TrafficLightType trafficLightType) {
+        if (trafficLightType == TrafficLightType.ACTUATION) {
+            return actuatedTimingsOptimiser.getMaxGroupTimings();
+        } else if (trafficLightType == TrafficLightType.FIXEDCYCLE) {
+            return fixedTimingsOptimiser.getMaxGroupTimings();
+        }
+        return new int[0]; //if lights r invalid, shouldn't ever really get to this 
+    }
+
+
+    public int[] getMinGroupTimings(TrafficLightType trafficLightType) {
+        if (trafficLightType == TrafficLightType.ACTUATION) {
+            return actuatedTimingsOptimiser.getMinGroupTimings();
+        }
+        return new int[0]; 
+    }
+
+    public double getCycleTime() {
+        return cycleTimeOptimiser.getCycleTime();
+    }
+
+
+    public void optimiseTrafficLights() {
+        TrafficLightType trafficLightType = TrafficLightType.ACTUATION;
+        int[] maxTimings = getMaxGroupTimings(trafficLightType);
+        int[] minTimings = getMinGroupTimings(trafficLightType);
+        double cycleTime = getCycleTime();
+
+   // this is just for debugging, dont acc need this in the end  
+        System.out.println("Optimising traffic lights of type: " + trafficLightType);
+        System.out.println("Max Timings: " + Arrays.toString(maxTimings));
+        System.out.println("Min Timings: " + Arrays.toString(minTimings));
+        System.out.println("Cycle Time: " + cycleTime);
+    }
+}
