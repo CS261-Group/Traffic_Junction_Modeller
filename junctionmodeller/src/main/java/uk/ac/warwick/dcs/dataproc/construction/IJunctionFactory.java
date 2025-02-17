2e6cb198f972243519e941b3e5c49efb3933a1f0
David R <david2005.rosental@gmail.com>
2025-02-17 02:03:29 +0000

    Created junction configuration creation process from UI data.
    Still missing error catching for unassigned things in the builders. I will do that next. It's all been TODO'd

--- junctionmodeller/src/main/java/uk/ac/warwick/dcs/dataproc/construction/IJunctionFactory.java
+++ junctionmodeller/src/main/java/uk/ac/warwick/dcs/dataproc/construction/IJunctionFactory.java
@@ -2,7 +2,8 @@ package uk.ac.warwick.dcs.dataproc.construction;
 
 import uk.ac.warwick.dcs.contracts.JunctionConfiguration;
 import uk.ac.warwick.dcs.contracts.exceptions.InvalidDirectionException;
+import uk.ac.warwick.dcs.contracts.exceptions.InvalidGroupNumberException;
 
 public interface IJunctionFactory<T> {
-    JunctionConfiguration createJunction(T data) throws InvalidDirectionException;
+    JunctionConfiguration createJunction(T data) throws InvalidDirectionException, InvalidGroupNumberException;
 }
