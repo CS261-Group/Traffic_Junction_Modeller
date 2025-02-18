6bb5b360372a468c56ef39e0a53a8278fdf8388f
JachymT <Jachym.tolar@gmail.com>
2025-02-18 22:29:04 +0000

    simple, generic, gradient descent algorithm

--- junctionmodeller/src/main/java/uk/ac/warwick/dcs/optimisation/GradientDescent.java
+++ junctionmodeller/src/main/java/uk/ac/warwick/dcs/optimisation/GradientDescent.java
@@ -1,25 +1,64 @@
 package uk.ac.warwick.dcs.optimisation;
 
-// slightly different from local search
-public class GradientDescent<S> {
+import org.ejml.simple.SimpleMatrix;
 
-    private int[] variables;
+// changing from beam search to gradient descent
+public class GradientDescent<F extends IGradientFunction> {
 
-    private int stepSize;
+    private final int ITERATIONS = 10;
+    private final double STEPSIZE = 0.1;
 
-    public GradientDescent(int[] v){
-        variables = v;
+    // column vector containing values for variables
+    private SimpleMatrix stateVector;
+    private SimpleMatrix maskVector;
+
+    // derivative of the cost function
+    private F NablaF;
+
+    public GradientDescent(float[] initialState, F gradientFunction, boolean[] variableMask){
+        stateVector = new SimpleMatrix(initialState);
+        NablaF = gradientFunction;
+        maskVector = this.maskArrayToMatrix(variableMask);
     }
 
-    //takes a copy of the current state
-    public S nextState(S currentState){
-
-        //for each variable
-        //use the partial derivative of the evaluation function
-        //at the current state
-        //to get the new value of the variable
-        return null;
-
+    public GradientDescent(float[] initialState, F gradientFunction){
+        stateVector = new SimpleMatrix(initialState);
+        NablaF = gradientFunction;
+        maskVector = this.createEmptyMask(initialState.length);
     }
+
+    // needs testing
+    public float[] getStateValues(){
+        return stateVector.getFDRM().getData();
+    }
+
+    public void nextState(){
+        SimpleMatrix gradientVector = new SimpleMatrix(NablaF.evaluateAt(stateVector));
+        stateVector = stateVector.minus(gradientVector.elementMult(maskVector).scale(STEPSIZE));
+    }
+
+    public float[] stepThrough(){
+        for (int i = 0; i < ITERATIONS; i++){
+            this.nextState();
+        }
+        return this.getStateValues();
+    }
+
+    // creates a nxn mask matrix from array
+    private SimpleMatrix maskArrayToMatrix(boolean[] variableMask){
+
+        SimpleMatrix mask = createEmptyMask(variableMask.length);
+
+        // TODO replace 1s with 0s in line with variableMask
+
+        return mask;
+    }
+
+    //no mask is just the identity matrix
+    private SimpleMatrix createEmptyMask(int length){
+        return SimpleMatrix.ones(1,length);
+    }
+
+
 
 }
