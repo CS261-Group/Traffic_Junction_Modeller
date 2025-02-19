8623d39055c05eea1b481f7b17d00e120a79f88e
satyan12 <satyan.bhundia@hotmail.com>
2025-02-19 18:20:25 +0000

    Update ActuatedTrafficLightObjectiveFunction.java

--- junctionmodeller/src/main/java/uk/ac/warwick/dcs/optimisation/ActuatedTrafficLightObjectiveFunction.java
+++ junctionmodeller/src/main/java/uk/ac/warwick/dcs/optimisation/ActuatedTrafficLightObjectiveFunction.java
@@ -12,10 +12,9 @@ public class ActuatedTrafficLightObjectiveFunction implements IGradientFunction 
 
     // Convert flow from vehicles per hour (vph) to vehicles per second (vps)
     private int convertFlowToVps(int flowVph) {
-        return flowVph / 3600;  // Convert from vehicles per hour to vehicles per second (integer division)
+        return flowVph / 3600;
     }
 
-    // Evaluate the objective function (total wait time) at the current state (timings)
     @Override
     public int evaluateAt(SimpleMatrix state) {
         int totalWaitTime = 0;
