17fbd0b04666b3afb34057b398457d9f906c82c0
JachymT <Jachym.tolar@gmail.com>
2025-02-27 12:34:23 +0000

    implemented calibration constant

--- junctionmodeller/src/main/java/uk/ac/warwick/dcs/evaluation/lanemetrics/ActuatedLaneMetricCalculator.java
+++ junctionmodeller/src/main/java/uk/ac/warwick/dcs/evaluation/lanemetrics/ActuatedLaneMetricCalculator.java
@@ -2,8 +2,10 @@ package uk.ac.warwick.dcs.evaluation.lanemetrics;
 
 public class ActuatedLaneMetricCalculator extends LaneMetricCalculator {
 
+    // 1.1c IN akcelik 2000
     @Override
     public double getCalibrationConstant(double sg) {
-        return 0;
+        return 0.1 * Math.pow(sg, 0.6);
+
     }
 }
--- junctionmodeller/src/main/java/uk/ac/warwick/dcs/evaluation/lanemetrics/FixedtimeLaneMetricCalculator.java
+++ junctionmodeller/src/main/java/uk/ac/warwick/dcs/evaluation/lanemetrics/FixedtimeLaneMetricCalculator.java
@@ -2,8 +2,9 @@ package uk.ac.warwick.dcs.evaluation.lanemetrics;
 
 public class FixedtimeLaneMetricCalculator extends LaneMetricCalculator {
 
+    // 1.1d IN akcelik 2000
     @Override
     public double getCalibrationConstant(double sg) {
-        return 0;
+        return 0.12 * Math.pow(sg, 0.7);
     }
 }
