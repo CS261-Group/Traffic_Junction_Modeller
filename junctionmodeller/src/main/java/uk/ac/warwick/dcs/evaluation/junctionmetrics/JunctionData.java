3b5b00b8987d508ad039a0a55fbb7d8db31700c1
JachymT <Jachym.tolar@gmail.com>
2025-03-01 07:37:50 +0000

    converts from junctionConfigs to the actual data we need

--- junctionmodeller/src/main/java/uk/ac/warwick/dcs/evaluation/junctionmetrics/JunctionData.java
+++ junctionmodeller/src/main/java/uk/ac/warwick/dcs/evaluation/junctionmetrics/JunctionData.java
@@ -1,6 +1,8 @@
 package uk.ac.warwick.dcs.evaluation.junctionmetrics;
 
 import uk.ac.warwick.dcs.contracts.JunctionConfiguration;
+import uk.ac.warwick.dcs.contracts.enums.Direction;
+import uk.ac.warwick.dcs.contracts.exceptions.InvalidDirectionException;
 import uk.ac.warwick.dcs.contracts.structure.Carriageway;
 import uk.ac.warwick.dcs.contracts.structure.IncomingLane;
 
@@ -8,28 +10,73 @@ public class JunctionData {
 
     public JunctionConfiguration junctionConfig;
 
-    // incoming flow is the same for all carriageways
+    /**
+     * Splits incoming flow equally among all lanes
+     * @param carriageway carriageway containing lanes
+     * @return the arrival flow rate
+     */
     public double getLaneArrivalFlow(Carriageway carriageway){
         return (double) carriageway.getIncomingFlow() / carriageway.getNumIncomingLanes();
     }
 
     /**
-     * Get the outgoing flow for a direction
-     * Get the number of lanes going in that direction
-     * the saturation flow for a lane
-     *
-     * @param carriageway
-     * @return
+     * The left direction is clockwise (the successor enum value)
+     * @param dir The input direction
+     * @return The direction of the left turn from the input direction
      */
-    public double getLaneSaturationFlow(Carriageway carriageway){
+    public Direction leftOf(Direction dir) {
+        return Direction.values()[(dir.ordinal() + 1) % 4];
+    }
 
-        IncomingLane[] left;
-        IncomingLane[] right;
-        IncomingLane[] ahead;
+    /**
+     * The right direction is anti-clockwise (the previous enum value)
+     * @param dir The input direction
+     * @return The direction of the right turn from the input direction
+     */
+    public Direction rightOf(Direction dir){
+        return Direction.values()[(dir.ordinal() - 1) % 4];
+    }
 
-        carriageway.itter
-        dir2;
-        dir 3
-        carriageway.getNumIncomingLanes()
+    /**
+     * The ahead direction is opposite (the second successor enum value)
+     * @param dir The input direction
+     * @return The direction of ahead from the input direction
+     */
+    public Direction aheadOf(Direction dir){
+        return Direction.values()[(dir.ordinal() + 2) % 4];
+    }
+
+    /**
+     * Determines the saturation flow for a lane.
+     * Note: Assumes the user inputs the outgoing flow for JUST ONE LANE in each direction
+     *
+     * @param carriageway A carriageway
+     * @param lane A lane belonging to the carriageway
+     * @return the saturation flow rate ascribed to the lane
+     */
+    public double laneSaturationFlow(Carriageway carriageway, IncomingLane lane){
+
+        Direction carriagewayDir = carriageway.getDirection();
+        //null initialising instead of throwing InvalidPerm
+        Direction prefferedFlowDirection; //null initialising instead of throwing InvalidPermi
+
+        if (lane.allowsGoing(rightOf(carriagewayDir))){
+            prefferedFlowDirection = rightOf(carriagewayDir);
+        }
+        else if (lane.allowsGoing(leftOf(carriagewayDir))){
+            prefferedFlowDirection = leftOf(carriagewayDir);
+        }
+        else if (lane.allowsGoing(aheadOf(carriagewayDir))) {
+            prefferedFlowDirection = aheadOf(carriagewayDir);
+        } else { //instead of throwing an InvalidPermittedDirectionsError, since that shouldn't be done here
+            prefferedFlowDirection = null;
+        }
+
+        try{
+            return carriageway.getOutgoingFlow(prefferedFlowDirection);
+        } catch (InvalidDirectionException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
