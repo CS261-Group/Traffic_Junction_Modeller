17a06a87657c015da0a022c69c38a659acea1346
JachymT <Jachym.tolar@gmail.com>
2025-03-01 16:50:15 +0000

    more accessors

--- junctionmodeller/src/main/java/uk/ac/warwick/dcs/contracts/timings/Groups.java
+++ junctionmodeller/src/main/java/uk/ac/warwick/dcs/contracts/timings/Groups.java
@@ -1,7 +1,11 @@
 package uk.ac.warwick.dcs.contracts.timings;
 
+import uk.ac.warwick.dcs.contracts.exceptions.InvalidGroupNumberException;
+import uk.ac.warwick.dcs.contracts.structure.IncomingLane;
+
 import java.util.Iterator;
 import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * Object used to store lanes that are part of each traffic
@@ -26,6 +30,13 @@ public class Groups implements Iterable<Group> {
      */
     public static final int MIN_NUM_GROUPS = 2;
 
+    // time between amber in one group to green in another
+    // TODO: source???
+    private static final double TRANSITION_TIME = 2.5;
+
+    // time to go through all groups
+    private double CycleTime;
+
     private final int numGroups;
     private final List<Group> groups;
     private final boolean optimiseTimings;
@@ -39,27 +50,69 @@ public class Groups implements Iterable<Group> {
         this.optimiseTimings = optimiseTimings;
 
         if (optimiseTimings) {
-            this.timings = null;
+            this.timings = null; // not good, should create a default timings list instead
         } else {
             assert timings != null;
             // sanity check: each group should have its own timing
             assert timings.size() == numGroups;
             this.timings = timings;
+            this.setCycleTime();
         }
+
+    }
+
+
+    private void setCycleTime(){
+        double sumTimings = 0;
+
+        for (GroupTiming timing : timings) {
+            sumTimings += timing.getTiming();
+        }
+
+        CycleTime = sumTimings + (TRANSITION_TIME * numGroups);
     }
 
     /**
-     *
      * @return The number of traffic light groups in the configuration.
      */
     public int getNumGroups() { return numGroups; }
 
     /**
-     *
      * @return Whether the group timings are being optimised in this configuration.
      */
     public boolean getOptimising() {
         return optimiseTimings;
+    }
+
+    // Not sure if I should throw an exception the input or just return 0
+    public double getLaneTiming(IncomingLane lane){
+        Iterator<Group> groupIterator = this.iterator();
+
+        while (groupIterator.hasNext()) {
+            Group group = groupIterator.next();
+            if (group.containsLane(lane)){
+
+                return getGroupTiming(group.getGroupNum()).getTiming();
+            }
+        }
+
+        return 0;
+    }
+
+    // Not sure if I should throw an exception the input or just return null
+    public GroupTiming getGroupTiming(int groupNum){
+        Iterator<GroupTiming> timingsIterator = timings.iterator();
+
+        while (timingsIterator.hasNext()) {
+            GroupTiming groupTiming = timingsIterator.next();
+
+            if (groupTiming.getGroupNum() == groupNum) {
+                return groupTiming;
+            }
+        }
+
+        //throw new InvalidGroupNumberException(groupNum, numGroups);
+        return null;
     }
 
     @Override
