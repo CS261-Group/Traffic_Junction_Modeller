f64e2f48940975762b63ad4331e795634225e0cb
David R <david2005.rosental@gmail.com>
2025-03-01 01:45:11 +0000

    Improved LaneDirectionSettingsPanel checkbox order

--- junctionmodeller/src/main/java/uk/ac/warwick/dcs/contracts/enums/Direction.java
+++ junctionmodeller/src/main/java/uk/ac/warwick/dcs/contracts/enums/Direction.java
@@ -1,5 +1,8 @@
 package uk.ac.warwick.dcs.contracts.enums;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 /**
  * Enum used to specify cardinal directions of:
  * - permitted lanes
@@ -9,5 +12,13 @@ public enum Direction {
     NORTH,
     EAST,
     SOUTH,
-    WEST
+    WEST;
+
+    public static Optional<Direction> valueOf(int value) {
+        return Arrays.stream(values())
+                .filter(legNo -> legNo.ordinal() == value)
+                .findFirst();
+    }
 }
+
+// https://stackoverflow.com/questions/11047756/getting-enum-associated-with-int-value
