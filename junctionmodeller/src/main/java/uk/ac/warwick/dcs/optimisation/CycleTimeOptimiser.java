b60b08cb4c85fb3d0aef0b319036b955c3daa681
JachymT <Jachym.tolar@gmail.com>
2025-03-02 14:43:12 +0000

    cycle time optimiser no longer iterative, began implementing

--- junctionmodeller/src/main/java/uk/ac/warwick/dcs/optimisation/CycleTimeOptimiser.java
+++ junctionmodeller/src/main/java/uk/ac/warwick/dcs/optimisation/CycleTimeOptimiser.java
@@ -1,41 +1,29 @@
 package uk.ac.warwick.dcs.optimisation;
 
+/**
+ * Static (Does not do any kind of iterative optimisation)
+ * optimiser used to initialise green times later
+ * See <a href="https://www.sciencedirect.com/science/article/pii/B9780128153024000030">Chow, Ampountolas</a>
+ */
 public class CycleTimeOptimiser {
-//    private final int MIN_CYCLE_TIME = 32;
-//    private final int LOST_TIME = 2;
-//
-//    private int numTrafficLightGroups;
-//    private int[][] groupIncomingFlows;  // Incoming traffic flow for each group (vph)
-//    private int[][] groupMaxOutgoingFlow;  // Maximum possible flow (saturation) for each group (vph)
-//
-//    // Constructor
-//    public CycleTimeOptimiser(int numTrafficLightGroups, int[][] groupIncomingFlows, int[][] groupMaxOutgoingFlow) {
-//        this.numTrafficLightGroups = numTrafficLightGroups;
-//        this.groupIncomingFlows = groupIncomingFlows;
-//        this.groupMaxOutgoingFlow = groupMaxOutgoingFlow;
-//    }
-//
-//    // Get optimized cycle times
-//    public int[] getOptimizedCycleTimes() {
-//        // Initial cycle times (can be set to equal distribution across groups initially)
-//        int[] initialTimings = new int[numTrafficLightGroups];
-//        for (int i = 0; i < numTrafficLightGroups; i++) {
-//            initialTimings[i] = MIN_CYCLE_TIME + LOST_TIME;  // Minimum cycle time + some lost time
-//        }
-//
-//        // Create the gradient function for optimizing cycle times
-//        CycleTimeObjectiveFunction gradientFunction = new CycleTimeObjectiveFunction(groupIncomingFlows, groupMaxOutgoingFlow);
-//
-//        // Use Gradient Descent to optimize cycle times
-//        GradientDescent<CycleTimeObjectiveFunction> optimizer = new GradientDescent<>(initialTimings, gradientFunction);
-//
-//        // Run the optimization process
-//        optimizer.stepThrough();
-//
-//        // Get the optimized cycle times
-//        int[] optimalTimings = optimizer.getStateValues();
-//
-//        // Convert the timings to integers and return them
-//        return optimalTimings;
-//    }
+    public CycleTimeOptimiser(){};
+
+    /**
+     * y is an array of flow ratios, one for each group
+     */
+    private static double getSumFlowRatios(double[] y){
+        double Y = 0;
+            for (int i = 0; i < y.length; i++){
+                Y += y[i];
+            }
+        return Y;
+    }
+
+    /**
+     * 5.3 in Chow, Ampountolas
+     * L is the total lost time
+     */
+    public static double getCycleTime(double L, double[] y){
+        return (1.5*L + 5) / 1 - getSumFlowRatios(y);
+    }
 }
