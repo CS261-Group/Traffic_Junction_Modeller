2cb08f28b03ef86055fa89a517cf23fff91fe92b
David R <david2005.rosental@gmail.com>
2025-03-03 14:57:41 +0000

    Organisation

--- junctionmodeller/src/main/java/uk/ac/warwick/dcs/visualisation/buttons/TabsButton.java
+++ junctionmodeller/src/main/java/uk/ac/warwick/dcs/visualisation/buttons/TabsButton.java
@@ -3,5 +3,6 @@ package uk.ac.warwick.dcs.visualisation.buttons;
 public class TabsButton extends UiButton {
     public TabsButton() {
         super("/tab.png");
+        // TODO: popup menu
     }
 }
---
+++ junctionmodeller/src/main/java/uk/ac/warwick/dcs/visualisation/menus/TabsMenu.java
@@ -0,0 +1,17 @@
+package uk.ac.warwick.dcs.visualisation.menus;
+
+import javafx.scene.layout.VBox;
+import uk.ac.warwick.dcs.visualisation.ModelVisualisation;
+
+import java.awt.*;
+import java.util.List;
+
+public class TabsMenu extends VBox {
+    private final List<ModelVisualisation> modelVisualisations;
+
+    public TabsMenu(List<ModelVisualisation> visualisations, Button tabBtn) {
+        modelVisualisations = visualisations;
+
+        // TODO: implement listing
+    }
+}
