0dc85680968eb71badbfc9968c7de5fbdb9d17d8
JachymT <Jachym.tolar@gmail.com>
2025-03-04 08:31:43 +0000

    some hill climbing

--- junctionmodeller/src/main/java/uk/ac/warwick/dcs/contracts/timings/GroupTiming.java
+++ junctionmodeller/src/main/java/uk/ac/warwick/dcs/contracts/timings/GroupTiming.java
@@ -14,7 +14,7 @@ public class GroupTiming {
     /**
      * The minimum time (in secs) a traffic light can be active (green) for.
      */
-    public static final int MIN_GROUP_TIMING = 15;
+    public static final int MIN_GROUP_TIMING = 5;
 
     /**
      * The maximum time (in secs) a traffic light can be active (green) for.
