c858e8f80269d843cd9f1878a2f774489bb62543
eyhli <eyhlim@gmail.com>
2025-03-05 18:04:53 +0000

    finished DataService, had to save all files

--- junctionmodeller/src/main/java/uk/ac/warwick/dcs/dataproc/FileSaverService.java
+++ junctionmodeller/src/main/java/uk/ac/warwick/dcs/dataproc/FileSaverService.java
@@ -3,12 +3,11 @@ import java.util.List;
 
 import uk.ac.warwick.dcs.contracts.JunctionConfiguration;
 import uk.ac.warwick.dcs.dataproc.saving.Saver;
-import uk.ac.warwick.dcs.dataproc.validation.IValidator;
 
 class FileSaverService implements ISaverService{
     @Override
-    public List<String> save(IValidator validator, JunctionConfiguration junctionConfig, String configName){
-        Saver saver = new Saver(validator);
+    public List<String> save(JunctionConfiguration junctionConfig, String configName){
+        Saver saver = new Saver();
         saver.save(junctionConfig, configName);
         return List.of(saver.getSaveErrors());
     }
