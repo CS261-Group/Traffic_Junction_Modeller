b4aa2c8db8cbee726cf764c76121871e296ae96b
JachymT <Jachym.tolar@gmail.com>
2025-03-06 09:13:22 +0000

    consistency change

--- junctionmodeller/src/main/java/uk/ac/warwick/dcs/evaluation/junctiondata/GroupData.java
+++ junctionmodeller/src/main/java/uk/ac/warwick/dcs/evaluation/junctiondata/GroupData.java
@@ -27,7 +27,7 @@ public class GroupData implements Iterable<LaneData>{
         return lanes.iterator();
     }
 
-    public double maxFlowRatio(){
+    private double maxFlowRatio(){
         double maxFlowRatio = 0;
         for (LaneData lane : lanes){
             if (lane.getFlowRatio() > maxFlowRatio){
