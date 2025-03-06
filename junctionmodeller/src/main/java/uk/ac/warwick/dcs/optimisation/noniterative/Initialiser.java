c4328703c078c78894728e3766ad442760f9fd5a
JachymT <Jachym.tolar@gmail.com>
2025-03-06 10:33:55 +0000

    empty initialiser class

--- junctionmodeller/src/main/java/uk/ac/warwick/dcs/optimisation/noniterative/Initialiser.java
+++ junctionmodeller/src/main/java/uk/ac/warwick/dcs/optimisation/noniterative/Initialiser.java
@@ -2,13 +2,6 @@ package uk.ac.warwick.dcs.optimisation.noniterative;
 
 public abstract class Initialiser {
 
-    CycleTimeOptimiser cycleTimeOptimiser = new CycleTimeOptimiser();
-    GreenTimeEstimator greenTimeEstimator = new GreenTimeEstimator();
-
-    double[] maxFlowRatios = junctionData.getMaxFlowRatioForEachGroup();
-    double sumFlowRatios = getSumFlowRatios(maxFlowRatios);
-
-
     /**
      * y is an array of flow ratios, one for each group
      */
